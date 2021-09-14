1e940289ee96f2ff527f0adf0dc323d437d7ec3f
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-14 13:49:29 -0600

    Done with house model

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
 import { CarSchema } from '../models/Car.js'
+import { HouseSchema } from '../models/House.js'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Cars = mongoose.model('Car', CarSchema);
+  Houses = mongoose.model('House', HouseSchema)
 }
 
 export const dbContext = new DbContext()
