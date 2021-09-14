3475c5e827ff4403356a9046f602b81369bfcf72
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-14 13:30:50 -0600

    first commit

---
+++ client/app/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'kambrodian.us.auth0.com'
+export const audience = 'https://Codeworks-Projects.com'
+export const clientId = 'D0EifSAMVS1uhGrDLJahpj4YDyhwNeXl'
