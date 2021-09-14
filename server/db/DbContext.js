e5fece7925cd911c75854a781909e30e5133c6e7
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-14 13:41:01 -0600

    Cars are posting to server

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -6,7 +6,7 @@ import { CarSchema } from '../models/Car.js'
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Cars = mongoose.model('Car', CarSchema)
+  Cars = mongoose.model('Car', CarSchema);
 }
 
 export const dbContext = new DbContext()
